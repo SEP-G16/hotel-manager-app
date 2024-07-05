89ee046d0b286c98ec3c36f7cc89600198da9e58
VenuraKS25 <vks250102@gmail.com>
2024-07-06 02:24:14 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:hotel_manager/views/loading_screen.dart';
+import 'package:hotel_manager/views/login_screen.dart';
+import 'package:hotel_manager/views/profile_screen.dart';
+
+void main() {
+  runApp(const HotelManager());
+}
+
+class HotelManager extends StatelessWidget {
+  const HotelManager({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: ProfileScreen(),
+    );
+  }
+}
+
