7dea24130ae50441b3d40e4aa23074c009446740
Devindie Samaranayake <gdcsamaranayake2001@gmail.com>
2024-08-21 12:00:11 +0530

    added a window to review before pushing a temporary booking to a confirmed booking

--- lib/constants/colour_constants.dart
+++ lib/constants/colour_constants.dart
@@ -9,4 +9,6 @@ class ColourConstants{
   static const Color orchidAccent= Color(0xFFF9F7FF);
   static const Color ivory = Color(0xFFFFFDFD);
   static const Color brickRed = Color(0xFFBB3E48);
+  static const Color green = Color(0xFF47891D);
+  static const Color red = Color(0xFFFE1C07);
 }
