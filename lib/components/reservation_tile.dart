817076409e1217d610bc9f885aa50adbe21728fe
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-07 15:20:56 +0530

    added gesture to all functionalities

--- lib/components/reservation_tile.dart
+++ lib/components/reservation_tile.dart
@@ -9,120 +9,115 @@ import 'action_button.dart';
 
 class ReservationTile extends StatelessWidget {
   const ReservationTile(
-      {required this.reservation, required this.onArrowTap});
+      {required this.reservation, required this.onTap});
 
   final Reservation reservation;
-  final void Function() onArrowTap;
+  final void Function() onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(10.0),
-      margin: EdgeInsets.symmetric(
-        vertical: 10,
-        horizontal: 20,
-      ),
-      decoration: BoxDecoration(
-        color: ColourConstants.orchidAccent,
-        borderRadius: BorderRadius.circular(10.0),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
-            spreadRadius: 1,
-            blurRadius: 4,
-            offset: const Offset(0, 3), // changes position of shadow
-          ),
-        ],
-      ),
-      child: Row(
-        children: [
-          Expanded(
-            flex: 2,
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        reservation.customerName,
-                        overflow: TextOverflow.ellipsis,
-                        style: TextConstants.subTextStyle(fontSize: 18),
-                      ),
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        'Phone No. : ${reservation.phoneNo}',
-                        overflow: TextOverflow.ellipsis,
-                        style: TextConstants.subTextStyle(fontSize: 16),
-                      ),
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        'Room Type : ${reservation.roomType.getRoomTypeAsString()}',
-                        overflow: TextOverflow.ellipsis,
-                        style: TextConstants.subTextStyle(fontSize: 16),
-                      ),
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        'Room Count : ${reservation.roomCount}',
-                        overflow: TextOverflow.ellipsis,
-                        style: TextConstants.subTextStyle(fontSize: 16),
-                      ),
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        'Check In Date : ${DateFormat('yyyy/MM/dd').format(reservation.checkinDate)}',
-                        overflow: TextOverflow.ellipsis,
-                        style: TextConstants.subTextStyle(fontSize: 16),
-                      ),
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        'Checkout Date : ${DateFormat('yyyy/MM/dd').format(reservation.checkoutDate)}',
-                        overflow: TextOverflow.ellipsis,
-                        style: TextConstants.subTextStyle(fontSize: 16),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.all(10.0),
+        margin: EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 20,
+        ),
+        decoration: BoxDecoration(
+          color: ColourConstants.orchidAccent,
+          borderRadius: BorderRadius.circular(10.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 1,
+              blurRadius: 4,
+              offset: const Offset(0, 3), // changes position of shadow
             ),
-          ),
-          // Expanded(child: Column(
-          //   children: [
-          //     ActionButton(btnText: 'View', onTap: onViewBtnTap, height: 50, width: 100,),
-          //   ],
-          // ),),
-          IconButton(
-            onPressed: onArrowTap,
-            icon: Icon(
+          ],
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              flex: 2,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          reservation.customerName,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextConstants.subTextStyle(fontSize: 18),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          'Phone No. : ${reservation.phoneNo}',
+                          overflow: TextOverflow.ellipsis,
+                          style: TextConstants.subTextStyle(fontSize: 16),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          'Room Type : ${reservation.roomType.getRoomTypeAsString()}',
+                          overflow: TextOverflow.ellipsis,
+                          style: TextConstants.subTextStyle(fontSize: 16),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          'Room Count : ${reservation.roomCount}',
+                          overflow: TextOverflow.ellipsis,
+                          style: TextConstants.subTextStyle(fontSize: 16),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          'Check In Date : ${DateFormat('yyyy/MM/dd').format(reservation.checkinDate)}',
+                          overflow: TextOverflow.ellipsis,
+                          style: TextConstants.subTextStyle(fontSize: 16),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          'Checkout Date : ${DateFormat('yyyy/MM/dd').format(reservation.checkoutDate)}',
+                          overflow: TextOverflow.ellipsis,
+                          style: TextConstants.subTextStyle(fontSize: 16),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Icon(
               Icons.arrow_forward_ios_rounded,
               color: ColourConstants.mainBlue,
               size: 35,
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
