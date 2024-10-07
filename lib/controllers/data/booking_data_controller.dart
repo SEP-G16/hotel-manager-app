efd3397914bd12153d987818d81fde75dadc0125
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-07 15:12:25 +0530

    controller modified for fetching rooms and adding bookings

--- lib/controllers/data/booking_data_controller.dart
+++ lib/controllers/data/booking_data_controller.dart
@@ -1,45 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:hotel_manager/enum/room_type.dart';
 import 'package:hotel_manager/models/reservation.dart';
 import 'package:hotel_manager/models/booking.dart';
+import 'package:hotel_manager/models/room.dart';
 
 import '../../exception/network_exception.dart';
 import '../network/booking_network_controller.dart';
 
-class BookingDataController extends GetxController{
+class BookingDataController extends GetxController {
   static final BookingDataController instance = Get.find();
 
   final BookingNetworkController _bnc = BookingNetworkController.instance;
 
   RxList<Reservation> _tempBookingList = <Reservation>[].obs;
   List<Reservation> get tempBookingList => _tempBookingList;
+  RxList<Reservation> listenableReservationList = <Reservation>[].obs;
 
   RxList<Booking> _bookingList = <Booking>[].obs;
   List<Booking> get bookingList => _bookingList;
+  RxList<Booking> listenableBookingList = <Booking>[].obs;
 
   BookingDataController._();
 
   static Future<BookingDataController> create() async {
     final BookingDataController controller = BookingDataController._();
-    controller._initController();
+    await controller._initController();
     return controller;
   }
 
-  Future<void> reinitController() async
-  {
+  Future<void> reinitController() async {
     await _initController();
   }
 
   Future<void> _initController() async {
-    _tempBookingList.value = await _getTempBookings();
-    _bookingList.value = await _getAcceptedBookings();
+    List<Reservation> tempBookingList = await _getTempBookings();
+    _tempBookingList.assignAll(tempBookingList);
+    listenableReservationList.assignAll(tempBookingList);
+
+    List<Booking> acceptedBookingList = await _getAcceptedBookings();
+    _bookingList.assignAll(acceptedBookingList);
+    listenableBookingList.assignAll(acceptedBookingList);
   }
 
   Future<List<Reservation>> _getTempBookings() async {
     try {
       List<Map<String, dynamic>> tempBookingsMapList =
-      await _bnc.getTempBookings();
-      return tempBookingsMapList.map((map) => Reservation.fromMap(map)).toList();
+          await _bnc.getTempBookings();
+      return tempBookingsMapList
+          .map((map) => Reservation.fromMap(map))
+          .toList();
     } on NetworkException catch (e) {
       rethrow;
     } catch (e) {
@@ -50,8 +60,10 @@ class BookingDataController extends GetxController{
   Future<List<Booking>> _getAcceptedBookings() async {
     try {
       List<Map<String, dynamic>> acceptedBookingsMapList =
-      await _bnc.getAcceptedBookings();
-      return acceptedBookingsMapList.map((map) => Booking.fromMap(map)).toList();
+          await _bnc.getAcceptedBookings();
+      return acceptedBookingsMapList
+          .map((map) => Booking.fromMap(map))
+          .toList();
     } on NetworkException catch (e) {
       rethrow;
     } catch (e) {
@@ -61,7 +73,7 @@ class BookingDataController extends GetxController{
 
   Future<void> acceptBooking({required int tempBookingId}) async {
     try {
-       //check sort
+      //check sort
     } on NetworkException catch (e) {
       rethrow;
     } catch (e) {
@@ -70,8 +82,64 @@ class BookingDataController extends GetxController{
   }
 
   Future<void> rejectBooking({required int tempBookingId}) async {
-    try {
+    try {} on NetworkException catch (e) {
+      rethrow;
+    } catch (e) {
+      rethrow;
+    }
+  }
 
+  Future<String> getAvailabilityStatus(
+      DateTime from, DateTime to, RoomType roomType) async {
+    try {
+      List<Map<String, dynamic>> availabilityMapList =
+          await _bnc.getAvailableRoomCount(from: from, to: to);
+      Map<String, dynamic> availabilityMap = availabilityMapList
+          .firstWhere((map) => map['type']! == roomType.getRoomTypeAsString());
+      return availabilityMap['roomCount']! != 0 ? 'Available' : 'Unavailable';
+    } on NetworkException catch (e) {
+      rethrow;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<List<Room>> getAvailableRoomsList(
+      DateTime from, DateTime to, RoomType roomType) async {
+    try {
+      List<Map<String, dynamic>> availableRoomList = await _bnc
+          .getAvailableRoomsList(from: from, to: to, roomType: roomType);
+      return availableRoomList.map((map) {
+        print(map);
+        // Map<String, dynamic> edited = {};
+        // edited['id'] = map['roomId']!;
+        // edited['roomNo'] = map['roomNo']!;
+        // edited['roomType'] = roomType.getRoomTypeAsString();
+        map['roomType'] = roomType.getRoomTypeAsString();
+        return Room.fromAvailabilityMap(map);
+      }).toList();
+    } on NetworkException catch (e) {
+      rethrow;
+    } catch (e) {
+      print(e);
+      rethrow;
+    }
+  }
+
+  Future<void> addBookingByReservation({required Reservation reservation, required List<Room> roomList}) async {
+    try {
+      Booking booking = Booking.fromReservation(reservation, roomList);
+      Map<String, dynamic> bookingMap = booking.toMap();
+      bookingMap['id'] = null;
+      Map<String, dynamic> newBookingMap = await _bnc.addBooking(map : bookingMap);
+
+      await _bnc.removeReservation(reservationId : reservation.id);
+
+      _bookingList.add(Booking.fromMap(newBookingMap));
+      listenableBookingList.add(Booking.fromMap(newBookingMap));
+
+      _tempBookingList.removeWhere((existingReservation) => existingReservation.id == reservation.id);
+      listenableReservationList.removeWhere((existingReservation) => existingReservation.id == reservation.id);
     } on NetworkException catch (e) {
       rethrow;
     } catch (e) {
--- lib/controllers/network/booking_network_controller.dart
+++ lib/controllers/network/booking_network_controller.dart
@@ -2,33 +2,158 @@ import 'dart:convert';
 
 import 'package:get/get.dart';
 import 'package:hotel_manager/constants/network_constants.dart';
+import 'package:hotel_manager/controllers/data/auth_controller.dart';
 import 'package:hotel_manager/enum/review_status.dart';
 import 'package:hotel_manager/exception/network_exception.dart';
+import 'package:hotel_manager/exception/token_expired_exception.dart';
+import 'package:hotel_manager/exception/unauthorized_exception.dart';
 import 'package:http/http.dart' as http;
 
+import '../../enum/room_type.dart';
+import '../../exception/token_not_found_exception.dart';
+
 class BookingNetworkController extends GetxController {
+  final AuthController _authController = AuthController.instance;
+
   static BookingNetworkController instance = Get.find();
 
   Future<List<Map<String, dynamic>>> getTempBookings() async {
+    if (_authController.token == null) {
+      throw TokenNotFoundException('Token not found');
+    }
+
     try {
       var response = await http.get(
         Uri.parse('${NetworkConstants.baseUrl}/api/booking/temp/all'),
+        headers: {
+          'Authorization': 'Bearer ${_authController.token!}',
+        },
       );
+
+      if (response.statusCode == 401) {
+        await _authController.logout();
+        throw TokenExpiredException('Token expired');
+      }
+
+      if (response.statusCode != 200) {
+        throw NetworkException('Failed to fetch temp bookings');
+      }
+      List<dynamic> decoded = jsonDecode(response.body);
+      return decoded.map<Map<String, dynamic>>((element) {
+        Map<String, dynamic> map = element as Map<String, dynamic>;
+        return map;
+      }).toList();
+    } on FormatException catch (e) {
+      print(e);
+      rethrow;
+    }
+  }
+
+  Future<List<Map<String, dynamic>>> getAcceptedBookings() async {
+    if (_authController.token == null) {
+      throw TokenNotFoundException('Token not found');
+    }
+
+    try {
+      var response = await http.get(
+        Uri.parse('${NetworkConstants.baseUrl}/api/booking/all'),
+        headers: {
+          'Authorization': 'Bearer ${_authController.token!}',
+        },
+      );
+
+      if (response.statusCode == 401) {
+        await _authController.logout();
+        throw TokenExpiredException('Token expired');
+      }
+
+      if (response.statusCode != 200) {
+        print(response.body);
+        throw NetworkException('Failed to fetch temp bookings');
+      }
       List<dynamic> decoded = jsonDecode(response.body);
       print(decoded);
       return decoded.map<Map<String, dynamic>>((element) {
         Map<String, dynamic> map = element as Map<String, dynamic>;
         return map;
       }).toList();
-    } catch (e) {
-      throw NetworkException(e.toString());
+    } on FormatException catch (e) {
+      print(e);
+      rethrow;
     }
   }
 
-  Future<List<Map<String, dynamic>>> getAcceptedBookings() async {
-    List<Map<String, dynamic>> dataList = [];
-    //
-    return dataList;
+  Future<List<Map<String, dynamic>>> getAvailableRoomCount(
+      {required DateTime from, required DateTime to}) async {
+    if (_authController.token == null) {
+      throw TokenNotFoundException('Token not found');
+    }
+
+    try {
+      var response = await http.get(
+        Uri.parse(
+            '${NetworkConstants.baseUrl}/api/room-type/available-count?from=${from.toIso8601String().split('T').first}&to=${to.toIso8601String().split('T').first}'),
+        headers: {
+          'Authorization': 'Bearer ${_authController.token!}',
+        },
+      );
+
+      if (response.statusCode == 401) {
+        await _authController.logout();
+        throw TokenExpiredException('Token expired');
+      }
+
+      if (response.statusCode != 200) {
+        print(response.body);
+        throw NetworkException('Failed to fetch temp bookings');
+      }
+      List<dynamic> decoded = jsonDecode(response.body);
+      print(decoded);
+      return decoded.map<Map<String, dynamic>>((element) {
+        Map<String, dynamic> map = element as Map<String, dynamic>;
+        return map;
+      }).toList();
+    } on FormatException catch (e) {
+      print(e);
+      rethrow;
+    }
+  }
+
+  Future<List<Map<String, dynamic>>> getAvailableRoomsList(
+      {required DateTime from,
+      required DateTime to,
+      required RoomType roomType}) async {
+    if (_authController.token == null) {
+      throw TokenNotFoundException('Token not found');
+    }
+
+    try {
+      var response = await http.get(
+        Uri.parse(
+            '${NetworkConstants.baseUrl}/api/room/available-rooms?from=${from.toIso8601String().split('T').first}&to=${to.toIso8601String().split('T').first}&roomTypeId=${roomType.getTypeId()}'),
+        headers: {
+          'Authorization': 'Bearer ${_authController.token!}',
+        },
+      );
+
+      if (response.statusCode == 401) {
+        await _authController.logout();
+        throw TokenExpiredException('Token expired');
+      }
+
+      if (response.statusCode != 200) {
+        throw NetworkException('Failed to fetch available room count');
+      }
+      List<dynamic> decoded = jsonDecode(response.body);
+      print(decoded);
+      return decoded.map<Map<String, dynamic>>((element) {
+        Map<String, dynamic> map = element as Map<String, dynamic>;
+        return map;
+      }).toList();
+    } on FormatException catch (e) {
+      print(e);
+      rethrow;
+    }
   }
 
   Future<Map<String, dynamic>> acceptReview({required int tempReviewId}) async {
@@ -39,5 +164,70 @@ class BookingNetworkController extends GetxController {
 
   Future<void> rejectReview({required int tempReviewId}) async {
     //
+  }
+
+  Future<Map<String,dynamic>> addBooking({required Map<String, dynamic> map}) async {
+
+    print(map);
+
+    if (_authController.token == null) {
+      throw TokenNotFoundException('Token not found');
+    }
+
+    try {
+      var response = await http.post(
+        Uri.parse('${NetworkConstants.baseUrl}/api/booking/add'),
+        headers: {
+          'Authorization': 'Bearer ${_authController.token!}',
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(map),
+      );
+
+      if (response.statusCode == 401) {
+        await _authController.logout();
+        throw TokenExpiredException('Token expired');
+      }
+
+      if (!(response.statusCode >= 200 && response.statusCode < 300)) {
+        print(response.body);
+        throw NetworkException('Failed to add booking');
+      }
+      Map<String, dynamic> decoded = jsonDecode(response.body);
+      return decoded;
+    } on FormatException catch (e) {
+      print(e);
+      rethrow;
+    }
+  }
+
+  Future<void> removeReservation({required int reservationId}) async {
+    if (_authController.token == null) {
+      throw TokenNotFoundException('Token not found');
+    }
+
+    try {
+      var response = await http.delete(
+        Uri.parse('${NetworkConstants.baseUrl}/api/booking/temp/remove'),
+        headers: {
+          'Authorization': 'Bearer ${_authController.token!}',
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(reservationId),
+      );
+
+      if (response.statusCode == 401) {
+        await _authController.logout();
+        throw TokenExpiredException('Token expired');
+      }
+
+      if (!(response.statusCode >= 200 && response.statusCode < 300)) {
+        print(response.body);
+        throw NetworkException('Failed to remove booking');
+      }
+    } on FormatException catch (e) {
+      print(e);
+      rethrow;
+    }
   }
 }
