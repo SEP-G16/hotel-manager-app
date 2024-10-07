d58d12dfca4e4730623f6ce87af3aef18682c41f
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-07 15:13:49 +0530

    added new drawer and dashboard management state controllers

--- lib/controller_initalizer.dart
+++ lib/controller_initalizer.dart
@@ -1,8 +1,13 @@
 import 'package:get/get.dart';
+import 'package:hotel_manager/controllers/data/auth_controller.dart';
 import 'package:hotel_manager/controllers/data/booking_data_controller.dart';
 import 'package:hotel_manager/controllers/data/review_data_controller.dart';
+import 'package:hotel_manager/controllers/data/secure_storage_controller.dart';
+import 'package:hotel_manager/controllers/network/auth_network_controller.dart';
 import 'package:hotel_manager/controllers/network/booking_network_controller.dart';
 import 'package:hotel_manager/controllers/network/review_network_controller.dart';
+import 'package:hotel_manager/controllers/view/dashboard_state_controller.dart';
+import 'package:hotel_manager/controllers/view/drawer_state_controller.dart';
 
 class ControllerInitializer{
 
@@ -17,6 +22,8 @@ class ControllerInitializer{
       await Get.putAsync(() => BookingDataController.create());
       Get.put(ReviewNetworkController());
       await Get.putAsync(() => ReviewDataController.create());
+      Get.put(DrawerStateController());
+      Get.put(DashboardStateController());
     }catch(e){
       print(e.toString());
       rethrow;
