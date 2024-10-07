5ada166446e285a21f575b2546d78ed2d5752fd4
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-07 15:14:31 +0530

    modified dashboard ui

--- lib/views/dashboard_screen.dart
+++ lib/views/dashboard_screen.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:hotel_manager/components/action_button.dart';
+import 'package:hotel_manager/components/custom_drawer.dart';
 import 'package:hotel_manager/constants/colour_constants.dart';
 import 'package:hotel_manager/constants/svg_constants.dart';
 import 'package:hotel_manager/constants/text_constants.dart';
+import 'package:hotel_manager/controllers/view/dashboard_state_controller.dart';
+import 'package:hotel_manager/controllers/view/drawer_state_controller.dart';
 import 'package:hotel_manager/views/manage_staff/manage_staff_screen.dart';
 import 'package:hotel_manager/views/manage_reservations/manage_reservations_screen.dart';
 import 'package:hotel_manager/views/profile_screen.dart';
@@ -13,118 +16,241 @@ import 'package:hotel_manager/views/review_screen.dart';
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
 
-  String greetTextGenerator() {
-    String greeting = '';
-    int hour = DateTime.now().hour;
-    if (hour < 12) {
-      greeting = 'Good Morning';
-    } else if (hour < 3) {
-      greeting = 'Good Afternoon';
-    } else {
-      greeting = 'Good Evening';
-    }
-    return greeting;
-  }
-
   @override
   Widget build(BuildContext context) {
+    DrawerStateController.instance.selectedIndex = 0;
     return Scaffold(
+      drawer: CustomDrawer(),
       backgroundColor: ColourConstants.ivory,
       body: SafeArea(
         child: Container(
-          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10.0),
+          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10.0),
           alignment: Alignment.center,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               Stack(
+                alignment: Alignment.center,
                 children: [
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Text(
-                          '${greetTextGenerator()}\n${'Venura'}!', //Add username here
-                          style: TextConstants.mainTextStyle(
-                              color: ColourConstants.chineseBlack,
-                              fontSize: 30),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ],
-                  ),
                   Align(
-                    alignment: Alignment.centerRight,
-                    child: GestureDetector(
-                      onTap: (){
-                        Get.to(() => ProfileScreen());
-                      },
-                      child: Container(
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          border: Border.all(
-                              color: ColourConstants.mainBlue, width: 3),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(4.0),
+                    alignment: Alignment.centerLeft,
+                    child: Builder(
+                      builder: (context) {
+                        return GestureDetector(
+                          onTap: () {
+                            Scaffold.of(context).openDrawer();
+                          },
                           child: Icon(
-                            Icons.person_outline_rounded,
+                            Icons.menu_rounded,
                             size: 30,
-                            color: ColourConstants.mainBlue,
                           ),
-                        ),
-                      ),
+                        );
+                      }
                     ),
                   ),
-                ],
-              ),
-              Row(
-                children: [
-                  Expanded(
+                  Align(
+                    alignment: Alignment.center,
                     child: Text(
-                      'Time to manage your hotel!',
-                      style: TextConstants.mainTextStyle(fontSize: 28),
-                      textAlign: TextAlign.center,
+                      'Dashboard',
+                      style: TextConstants.mainTextStyle(fontSize: 32),
                     ),
                   ),
                 ],
               ),
               Expanded(
-                  child: SvgPicture.string(SvgConstants.dashboardScreenImage)),
-              ActionButton(
-                btnText: 'Manage My Staff',
-                onTap: () {
-                  Get.to(() => ManageStaffScreen());
-                },
-                width: 250,
-                outlineMode: true,
-                borderColour: ColourConstants.mainBlue,
-                borderWidth: 2.0,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              ActionButton(
-                btnText: 'Manage Hotel Bookings',
-                onTap: () {
-                  Get.to(() => ManageReservationsScreen());
-                },
-                width: 250,
-                outlineMode: true,
-                borderColour: ColourConstants.mainBlue,
-                borderWidth: 2.0,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              ActionButton(
-                btnText: 'Manage Customer Reviews',
-                onTap: () {
-                  Get.to(() => ReviewScreen());
-                },
-                width: 250,
-                outlineMode: true,
-                borderColour: ColourConstants.mainBlue,
-                borderWidth: 2.0,
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              'Stats',
+                              style: TextConstants.mainTextStyle(fontSize: 28),
+                              textAlign: TextAlign.left,
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: GestureDetector(
+                                    onTap: () {
+                                      DrawerStateController.instance.selectedIndex = 1;
+                                      Get.to(() => ManageReservationsScreen());
+                                    },
+                                    child: Container(
+                                      padding: EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+                                      margin: EdgeInsets.symmetric(horizontal: 5),
+                                      decoration: BoxDecoration(
+                                        color: ColourConstants.mainBlue,
+                                        borderRadius: BorderRadius.circular(5),
+                                      ),
+                                      child: Column(
+                                        children: [
+                                          Text(
+                                            'Reservations',
+                                            style: TextConstants.subTextStyle(
+                                                color: ColourConstants.white),
+                                          ),
+                                          Obx(
+                                            () => Text(
+                                              DashboardStateController.instance.reservationCount.toString(),
+                                              style: TextConstants.mainTextStyle(
+                                                  color: ColourConstants.white),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: GestureDetector(
+                                    onTap: () {
+                                      DrawerStateController.instance.selectedIndex = 1;
+                                      Get.to(() => ManageReservationsScreen());
+                                    },
+                                    child: Container(
+                                      padding: EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+                                      margin: EdgeInsets.symmetric(horizontal: 5),
+                                      decoration: BoxDecoration(
+                                        color: ColourConstants.mainBlue,
+                                        borderRadius: BorderRadius.circular(5),
+                                      ),
+                                      child: Column(
+                                        children: [
+                                          Text(
+                                            'Bookings',
+                                            style: TextConstants.subTextStyle(
+                                                color: ColourConstants.white),
+                                          ),
+                                          Obx(
+                                            () => Text(
+                                              DashboardStateController.instance.bookingCount.toString(),
+                                              style: TextConstants.mainTextStyle(
+                                                  color: ColourConstants.white),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: GestureDetector(
+                                    onTap: () {
+                                      DrawerStateController.instance.selectedIndex = 2;
+                                      Get.to(() => ReviewScreen());
+                                    },
+                                    child: Container(
+                                      padding: EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+                                      margin: EdgeInsets.symmetric(horizontal: 5),
+                                      decoration: BoxDecoration(
+                                        color: ColourConstants.mainBlue,
+                                        borderRadius: BorderRadius.circular(5),
+                                      ),
+                                      child: Column(
+                                        children: [
+                                          Text(
+                                            'Reviews',
+                                            style: TextConstants.subTextStyle(
+                                                color: ColourConstants.white),
+                                          ),
+                                          Obx(
+                                            () => Text(
+                                              DashboardStateController.instance.reviewCount.toString(),
+                                              style: TextConstants.mainTextStyle(
+                                                  color: ColourConstants.white),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),Expanded(
+                                  child: GestureDetector(
+                                    onTap: (){
+                                      DrawerStateController.instance.selectedIndex = 3;
+                                    },
+                                    child: Container(
+                                      padding: EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+                                      margin: EdgeInsets.symmetric(horizontal: 5),
+                                      decoration: BoxDecoration(
+                                        color: ColourConstants.mainBlue,
+                                        borderRadius: BorderRadius.circular(5),
+                                      ),
+                                      child: Column(
+                                        children: [
+                                          Text(
+                                            'Messages',
+                                            style: TextConstants.subTextStyle(
+                                                color: ColourConstants.white),
+                                          ),
+                                          Obx(
+                                            () => Text(
+                                              DashboardStateController.instance.messageCount.toString(),
+                                              style: TextConstants.mainTextStyle(
+                                                  color: ColourConstants.white),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 10,),
+                        Row(
+                          children: [
+                            Text(
+                              'Staff',
+                              style: TextConstants.mainTextStyle(fontSize: 28),
+                              textAlign: TextAlign.left,
+                            ),
+                          ],
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            DrawerStateController.instance.selectedIndex = 5;
+                          Get.to(() => ManageStaffScreen());},
+                          child: Container(
+                            padding: EdgeInsets.all(10),
+                            margin: EdgeInsets.symmetric(horizontal: 5),
+                            decoration: BoxDecoration(
+                              color: ColourConstants.white,
+                              borderRadius: BorderRadius.circular(10),
+                              border: Border.all(color: ColourConstants.mainBlue, width: 2),
+                            ),
+                            child: Row(
+                              children: [
+                                SvgPicture.string(SvgConstants.employeeGroupImage, height: 100,),
+                                SizedBox(width: 10,),
+                                Expanded(child: Text('Manage your Employees', style: TextConstants.subTextStyle(fontSize: 22), overflow: TextOverflow.visible,)),
+                              ],
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10,),
+                        SvgPicture.string(SvgConstants.dashboardScreenImage, width: MediaQuery.of(context).size.width * 0.7,),
+                        Text('Time to Start Managing your Hotel!', style: TextConstants.subTextStyle(), textAlign: TextAlign.center,),
+                      ],
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
