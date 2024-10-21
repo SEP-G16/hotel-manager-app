8042ce5565ba0d83f62fc816efbe32ab6db52e8d
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-22 00:08:42 +0530

    certificate issues fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:hotel_manager/constants/colour_constants.dart';
 import 'package:get/get.dart';
@@ -8,9 +10,19 @@ import 'controllers/data/secure_storage_controller.dart';
 import 'controllers/network/auth_network_controller.dart';
 
 
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
+
 // Initial comment
 void main() async {
-
+  HttpOverrides.global = MyHttpOverrides();
   Get.put(SecureStorageController());
   Get.put(AuthNetworkController());
 
